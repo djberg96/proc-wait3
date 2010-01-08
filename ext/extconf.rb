06d3314b914d61664558696fbcacfe29fa1b72c3
Daniel Berger <dberger@globe.gov>
2010-01-07 17:20:36 -0700

    Removed note regarding mkmf.rb and updated extconf.rb to define have_const if not already defined.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,6 +4,31 @@
 ########################################################
 require 'mkmf'
 
+# Not defined in older versions of Ruby.
+unless defined? have_const
+  def have_const(const, header = nil, opt = "", &b)
+     checking_for const do
+        header = cpp_include(header)
+        if try_compile(<<"SRC", opt, &b)
+#\{COMMON_HEADERS\}
+#\{header\}
+/* top */
+static int t = #\{const\};
+SRC
+           $defs.push(
+              format(
+                 "-DHAVE_CONST_%s",
+                 const.strip.upcase.tr_s("^A-Z0-9_", "_")
+              )
+           )
+           true
+        else
+           false
+        end
+     end
+  end
+end
+
 # Check to see if Ruby has already defined the various RLIMIT constants
 # and set an appropriate macro in the source.
 #
@@ -57,66 +82,18 @@ have_struct_member('struct siginfo', 'si_utime', 'signal.h')
 have_struct_member('struct siginfo', 'si_status', 'signal.h')
 have_struct_member('struct siginfo', 'si_stime', 'signal.h')
 
-count = 0
+have_const('P_CID', 'signal.h')
+have_const('P_GID', 'signal.h')
+have_const('P_MYID', 'signal.h')
+have_const('P_SID', 'signal.h')
+have_const('P_UID', 'signal.h')
 
-# Attempt to append the necessary method to the user's mkmf.rb file, after
-# backing it up first.
-begin
-   have_const('P_CID', 'signal.h')
-   have_const('P_GID', 'signal.h')
-   have_const('P_MYID', 'signal.h')
-   have_const('P_SID', 'signal.h')
-   have_const('P_UID', 'signal.h')
+have_const('P_ALL', 'signal.h') || have_const('P_ALL', 'sys/wait.h')
+have_const('P_PGID', 'signal.h') || have_const('P_PGID', 'sys/wait.h')
+have_const('P_PID', 'signal.h') || have_const('P_PID', 'sys/wait.h')
 
-   have_const('P_ALL', 'signal.h') || have_const('P_ALL', 'sys/wait.h')
-   have_const('P_PGID', 'signal.h') || have_const('P_PGID', 'sys/wait.h')
-   have_const('P_PID', 'signal.h') || have_const('P_PID', 'sys/wait.h')
-
-   # These are only supported by Solaris 8 and later afaik
-   have_const('P_PROJID', 'signal.h')
-   have_const('P_TASKID', 'signal.h')
-rescue NoMethodError
-   require 'rbconfig'
-   count += 1
-   file = File.join(Config::CONFIG['rubylibdir'], 'mkmf.rb')
-   FileUtils.cp(file, 'mkmf.orig') # Backup original
-   File.open(file, 'a+'){ |fh|
-      fh.puts %Q{
-# Returns whether or not the constant +const+ can be found in the common
-# header files, or within a +header+ that you provide. If found, a macro is
-# passed as a preprocessor constant to the compiler using the constant name,
-# in uppercase, prepended with 'HAVE_'. This method is also used to test
-# for the presence of enum values.
-# 
-# For example, if have_const('FOO') returned true, then the HAVE_CONST_FOO
-# preprocessor macro would be passed to the compiler.
-#
-# This method was added automatically by the proc-wait3 library on
-# #{Time.now}.
-#
-def have_const(const, header = nil, opt = "", &b)
-   checking_for const do
-      header = cpp_include(header)
-      if try_compile(<<"SRC", opt, &b)
-#\{COMMON_HEADERS\}
-#\{header\}
-/* top */
-static int t = #\{const\};
-SRC
-         $defs.push(
-            format(
-               "-DHAVE_CONST_%s",
-               const.strip.upcase.tr_s("^A-Z0-9_", "_")
-            )
-         )
-         true
-      else
-         false
-      end
-   end
-end}
-   }
-   retry unless count >= 2
-end
+# These are only supported by Solaris 8 and later afaik
+have_const('P_PROJID', 'signal.h')
+have_const('P_TASKID', 'signal.h')
 
 create_makefile('proc/wait3', 'proc')
