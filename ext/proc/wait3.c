febf0129b90d17746ba0f505f31388ddfa77c143
Daniel Berger <djberg96@gmail.com>
2014-04-24 09:21:25 -0600

    Version bump.

--- ext/proc/wait3.c
+++ ext/proc/wait3.c
@@ -966,8 +966,8 @@ void Init_wait3()
   rb_define_const(rb_mProcess, "P_PROJID", INT2FIX(P_PROJID));
 #endif
 
-  /* 1.7.0: The version of the proc-wait3 library */
-  rb_define_const(rb_mProcess, "WAIT3_VERSION", rb_str_new2("1.7.0"));
+  /* 1.7.1: The version of the proc-wait3 library */
+  rb_define_const(rb_mProcess, "WAIT3_VERSION", rb_str_new2("1.7.1"));
 
   /* Define this last in our Init_wait3 function */
   rb_define_readonly_variable("$last_status", &v_last_status);
