b264c11b44b14ecfd825d48f166eb32012bf010b
Daniel Berger <djberg96@gmail.com>
2020-10-02 10:26:11 -0400

    Bump version.

--- ext/proc/wait3.c
+++ ext/proc/wait3.c
@@ -954,8 +954,8 @@ void Init_wait3()
   rb_define_const(rb_mProcess, "RUSAGE_THREAD", INT2FIX(RUSAGE_THREAD));
 #endif
 
-  /* 1.8.1: The version of the proc-wait3 library */
-  rb_define_const(rb_mProcess, "WAIT3_VERSION", rb_str_freeze(rb_str_new2("1.8.1")));
+  /* 1.9.0: The version of the proc-wait3 library */
+  rb_define_const(rb_mProcess, "WAIT3_VERSION", rb_str_freeze(rb_str_new2("1.9.0")));
 
   /* Define this last in our Init_wait3 function */
   rb_define_readonly_variable("$last_status", &v_last_status);
