00df34da0e2c8543046e7dc55f90cefca320b8bb
Daniel Berger <djberg96@gmail.com>
2022-06-28 19:32:30 -0400

    Use single quoted strings.

--- examples/example_pause.rb
+++ examples/example_pause.rb
@@ -9,13 +9,13 @@
 require 'rbconfig'
 require 'proc/wait3'
 
-puts "Pausing.  Hit Ctrl-C to continue."
+puts 'Pausing.  Hit Ctrl-C to continue.'
 
 if Config::CONFIG['host_os'] =~ /linux/i
    Process.pause(2)
 else
-   Process.pause("INT")
+   Process.pause('INT')
 end
 
-puts "Hey, thanks for hitting Ctrl-C.  Continuing..."
-puts "Done"
+puts 'Hey, thanks for hitting Ctrl-C.  Continuing...'
+puts 'Done'
