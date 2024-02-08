8fa6ec93807d48cdba2591a3375a58eb7655932d
Daniel Berger <78529+djberg96@users.noreply.github.com>
2024-02-08 07:55:40 -0500

    Fix what I think is a copy/paste bug.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -53,20 +53,7 @@ have_struct_member('struct siginfo', 'si_stime', 'signal.h')
 
 have_const('P_CID', 'signal.h')
 have_const('P_GID', 'signal.h')
-have_const('P_MYID', 'signal.hclass PasswdStruct < FFI::Struct
-      layout(
-        :pw_name, :string,
-        :pw_passwd, :string,
-        :pw_uid, :uint,
-        :pw_gid, :uint,
-        :pw_change, :ulong,
-        :pw_class, :string,
-        :pw_gecos, :string,
-        :pw_dir, :string,
-        :pw_shell, :string,
-        :pw_expire, :ulong
-      )
-    end')
+have_const('P_MYID', 'signal.h')
 have_const('P_SID', 'signal.h')
 have_const('P_UID', 'signal.h')
 
